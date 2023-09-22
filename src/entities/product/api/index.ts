43229c2351c0cd513cfe4294673398566dbbafc6
strwatcher <starwatcher.focused@gmail.com>
2023-09-22 23:49:23 +1000

    move api in shared

--- src/entities/product/api/index.ts
+++
@@ -1,7 +0,0 @@
-export { createProductMutation } from "./create";
-export { getAllProductsQuery } from "./get-all";
-export { getProductQuery } from "./get-single";
-export { updateProductMutation } from "./update";
-export { deleteProductMutation } from "./delete";
-export { ProductContract } from "./common";
-export type { Product, ProductId, Products } from "./common";
